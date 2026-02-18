fc6abbfd06eb627c839f55887a18f13738e2d44c
Bushra Mansuri <bushmansuri11@gmail.com>
2026-02-18 17:00:41 +0530

    Enhance openSheet and sendProposal functions
    Refactor openSheet to accept iconElement and update icon on proposal send.

--- app.js
+++ app.js
@@ -2,39 +2,64 @@ const sheet = document.getElementById("sheet");
 const overlay = document.querySelector(".overlay");
 const profileName = document.getElementById("profileName");
 
-function openSheet (name){
-    profileName.innerText = name;
-    sheet.classList.add("active");
-    overlay.classList.add("active");
+let currentProfile = null;
+let currentIcon = null;
+
+function openSheet(name, iconElement) {
+  profileName.innerText = name;
+  sheet.classList.add("active");
+  overlay.classList.add("active");
+
+  currentProfile = name;
+  currentIcon = iconElement;
 }
 
-function closeSheet(){
-    sheet.classList.remove("active");
-    overlay.classList.remove("active");
+function closeSheet() {
+  sheet.classList.remove("active");
+  overlay.classList.remove("active");
 }
 
-function sendProposal(btn){
-    btn.classList.add("sent");
-    btn.innerHTML = "✔ Sent!";
-    createConfetti(btn);
+function sendProposal(btn) {
+  btn.classList.add("sent");
+  btn.innerHTML = "✔ Sent!";
+  createConfetti(btn);
 
+  /*setTimeout(() =>{
+    closeSheet();
+    
+},1200);
+*/
 
-setTimeout(() =>{
+  // Change only that specific card icon
+  if (currentIcon) {
+    currentIcon.innerHTML = `<i class="fa-solid fa-check"></i>`;
+    currentIcon.style.background = "#E76F51";
+  }
+
+  setTimeout(() => {
     closeSheet();
     btn.classList.remove("sent");
     btn.innerText = "Connect";
-},1200);
+  }, 1200);
 }
 
-function createConfetti(button){
-    for(let i=0; i<18; i++){
-        let conf = document.createElement("div");
-        conf.classList.add("confetti");
-        let x = (Math.random()-0.5)*200 + "px";
-        let y = (Math.random()-0.5)*200 + "px";
-        conf.style.setProperty("--x",x);
-        conf.style.setProperty("--y",y);
-        button.appendChild(conf);
-        setTimeout(() =>conf.remove(), 700);
-    }
+function createConfetti(button) {
+  for (let i = 0; i < 18; i++) {
+    let conf = document.createElement("div");
+    conf.classList.add("confetti");
+    let x = (Math.random() - 0.5) * 200 + "px";
+    let y = (Math.random() - 0.5) * 200 + "px";
+    conf.style.setProperty("--x", x);
+    conf.style.setProperty("--y", y);
+    button.appendChild(conf);
+    setTimeout(() => conf.remove(), 700);
+  }
 }
+
+
+
+
+
+
+
+
